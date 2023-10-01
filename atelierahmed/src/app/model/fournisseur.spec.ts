db48230a81d82ed3bc1c78ab73e3462f7fb534bd
hamouda123456 <TRABELSI.AHMED.1@ESPRIT.TN>
2023-10-01 21:57:21 +0200

    Initial commit

---
+++ atelierahmed/src/app/model/fournisseur.spec.ts
@@ -0,0 +1,8 @@
+import { Fournisseur } from './fournisseur';
+
+describe('Fournisseur', () => {
+  it('should create an instance', () => {
+ 
+    expect(new Fournisseur()).toBeTruthy();
+  });
+});
